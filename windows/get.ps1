b91728bbde6dedb4eeaa2af3c176574738a65ddf
Aaron Wright <aaron@sikosoft.com>
2019-11-20 23:42:02 +0100

    get updates repo

--- windows/get.ps1
+++ windows/get.ps1
@@ -3,10 +3,15 @@ param(
   [Parameter(Mandatory = $True, Position = 0, ValueFromPipeline = $false)][System.String]$configFile
 )
 
+$Currentlocation = Get-Location
+
+& "$PSScriptRoot\update-repo.ps1"
+
+Set-Location $Currentlocation
+
 if (Test-Path "$PSScriptRoot\..\config\$configFile") {
   Copy-Item -Path "$PSScriptRoot\..\config\$configFile" -Destination ".\$configFile"
   Write-Output "Copied $configFile to working directory."
 } else {
   Write-Output "$configFile does not exist!"
 }
-
